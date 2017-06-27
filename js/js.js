8cdc7ba357f60e4b95b5c347fe4889a30d56f923
zsh1007 <365873687@qq.com>
2017-06-27 15:14:25 +0800

    myBlog

--- js/js.js
+++ js/js.js
@@ -1,4 +1,10 @@
 /**
  * Created by zsh on 2017/6/27.
  */
-alert('1');
+$(document).ready(function(){
+    var nowDate=new Date();
+    var year=nowDate.getFullYear();
+    var month=nowDate.getMonth();
+    var day=nowDate.getDate();
+    $(".right-time").html(year+'-'+month+'-'+day);
+});
